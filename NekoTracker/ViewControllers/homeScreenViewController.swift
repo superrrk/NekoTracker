4fa147bae40929fc5fc9f3283115f1f277d8eb3a
Kelly <truong.kelly17@gmail.com>
2022-06-28 16:04:19 -0500

    Kelly 4:04pm

---
+++ NekoTracker/ViewControllers/homeScreenViewController.swift
@@ -0,0 +1,41 @@
+//
+//  homeScreenViewController.swift
+//  NekoTracker
+//
+//  Created by Scholar on 6/28/22.
+//
+
+import UIKit
+
+class homeScreenViewController: UIViewController {
+    
+    // ---------- labels --------------------------
+    @IBOutlet weak var imageViewCharacter: UIImageView!
+    @IBOutlet weak var logoNamePlaceholder: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    // ------------- buttons -----------------------
+    @IBAction func buttonTappedFood(_ sender: Any) {
+    }
+    @IBAction func buttonTappedSleep(_ sender: Any) {
+    }
+    @IBAction func buttonTappedMenstrual(_ sender: Any) {
+    }
+    @IBAction func buttonTappedWater(_ sender: Any) {
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
